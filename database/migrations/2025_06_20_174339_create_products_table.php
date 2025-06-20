3cc975a1b70ec290784bfb5b2b6e6e8afebb135e
Julian Grisales <jgrisales@gmail.com>
2025-06-20 20:42:53 +0200

    Migrations

--- database/migrations/2025_06_20_174339_create_products_table.php
+++ database/migrations/2025_06_20_174339_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedInteger('stock')->default(0);
+            $table->decimal('price', 10, 2)->default(0);
             $table->timestamps();
         });
     }
